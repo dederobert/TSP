ba96376338339c22ec9bcae5ad7102fb29d95c03
Axi57 <Axi57@users.noreply.github.com>
2017-03-03 15:26:43 +0100

    Update InfoSommetCarte.cpp

--- src/InfoSommetCarte.cpp
+++ src/InfoSommetCarte.cpp
@@ -1,13 +1,18 @@
 #pragma once
 #include "InfoSommetCarte.h"
 #include <sstream>
-
+/**
+	* iformation : name and position of the vertex <br />
+	*/
 InfoSommetCarte::operator std::string() const {
 	ostringstream oss;
 	oss << "nom = " << _nom << ", position = " << _position << endl;
 	return oss.str();
 }
 
+/**
+	* display with ostream operator <br />
+	*/
 ostream& operator<< (ostream& os, const InfoSommetCarte& infoSommetCarte) {
 	return os << (string)infoSommetCarte;
 }
