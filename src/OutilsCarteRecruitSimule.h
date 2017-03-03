c8ff987366f19ed1a5be1872f81f4e495398ed3e
Axi57 <Axi57@users.noreply.github.com>
2017-03-03 15:18:39 +0100

    Update OutilsCarteRecruitSimule.h

--- src/OutilsCarteRecruitSimule.h
+++ src/OutilsCarteRecruitSimule.h
@@ -7,8 +7,19 @@
 
 class OutilsCarteRecuitSimule {
 public:
+	
+	/**
+	* distance between two vertex <br />
+	*/
 	static double distance(const Sommet<InfoSommetCarte>*  s1,
 		const Sommet<InfoSommetCarte>* s2);
+	
+	/**
+	* creation of a graph
+	* @param sA information of the 1st vertex
+	* @param sB information of the 2nd vertex
+	* creation of the edge between the 2 vertex
+	*/
 	static Arete<InfoAreteCarte, InfoSommetCarte>* creerArete(Sommet<InfoSommetCarte>* sA,
 		Sommet<InfoSommetCarte>* sB,
 		Graphe<InfoAreteCarte, InfoSommetCarte>& graphe);
