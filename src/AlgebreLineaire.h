d643c7c3a8dc379bc7ebe216f799a2854680ffe9
Axi57 <Axi57@users.noreply.github.com>
2017-03-03 14:52:33 +0100

    Update AlgebreLineaire.h

--- src/AlgebreLineaire.h
+++ src/AlgebreLineaire.h
@@ -1,6 +1,9 @@
 #pragma once
 #include "Vector2D.h"
-
+/**
+	* Inline function norme <br />
+	* @param Vector2D vector
+	*/
 inline double norme(const Vector2D& A, const Vector2D& B) {
 	return sqrt(pow((A.x - B.x), 2) + pow((A.y - B.y),2))
 }
