8cb86934243889eb9cf56834a32c8d36edaeaebe
Axi57 <Axi57@users.noreply.github.com>
2017-03-03 15:32:46 +0100

    Update InfoAreteCarte.cpp

--- src/InfoAreteCarte.cpp
+++ src/InfoAreteCarte.cpp
@@ -1,13 +1,19 @@
 #pragma once
 #include "InfoAreteCarte.h"
 #include <sstream>
-
+/**
+	* Cost of the edge <br />
+	* @return oss.str() string with the cost
+	*/
 InfoAreteCarte::operator std::string() const {
 	ostringstream oss;
-	oss << "coût = " << _cout << endl;
+	oss << "coÅ±t = " << _cout << endl;
 	return oss.str();
 }
 
+	/**
+	* display with ostream operator <br />
+	*/
 ostream& operator<<(ostream& f, const InfoAreteCarte& infoAreteCarte) {
 	return f << (string)infoAreteCarte;
 }
